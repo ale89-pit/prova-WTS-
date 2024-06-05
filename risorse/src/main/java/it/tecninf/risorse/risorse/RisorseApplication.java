50a5c89c29f5d75453fb1071413a97a6e5435194
Alessio Pitorri <72412290+ale89-pit@users.noreply.github.com>
2024-06-05 15:08:25 +0200

    add risorse

---
+++ risorse/src/main/java/it/tecninf/risorse/risorse/RisorseApplication.java
@@ -0,0 +1,13 @@
+package it.tecninf.risorse.risorse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RisorseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RisorseApplication.class, args);
+	}
+
+}
---
+++ risorse/src/test/java/it/tecninf/risorse/risorse/RisorseApplicationTests.java
@@ -0,0 +1,13 @@
+package it.tecninf.risorse.risorse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RisorseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
